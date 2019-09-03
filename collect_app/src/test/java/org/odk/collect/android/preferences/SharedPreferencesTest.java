28468cdaa1fe781c61497c433b15769981019647
Callum Stott <callum@seadowg.com>
2019-09-03 15:26:11 +0100

    Move SharedPreferencesTest to Robolectric from Espresso
    Local timing results for old and new:
    
    Espresso - Build + install: 1m30s, Run: 89ms
    Robolectric - Build: 17s, Run: 337ms (without Robolectric startup time)
    
    Interesting part here is that for these Espresso to Robolectric moves we don't get a "run" time improvement but the actual process of making a change and then re-running a test should be pretty significant due to the faster build.

--- collect_app/src/test/java/org/odk/collect/android/preferences/SharedPreferencesTest.java
+++ collect_app/src/test/java/org/odk/collect/android/preferences/SharedPreferencesTest.java
@@ -1,29 +1,9 @@
-/*
- * Copyright 2017 Nafundi
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.odk.collect.android;
-
-import androidx.test.runner.AndroidJUnit4;
+package org.odk.collect.android.preferences;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.odk.collect.android.preferences.AdminSharedPreferences;
-import org.odk.collect.android.preferences.GeneralSharedPreferences;
-import org.odk.collect.android.preferences.GeneralKeys;
 import org.odk.collect.android.utilities.SharedPreferencesUtils;
+import org.robolectric.RobolectricTestRunner;
 
 import java.util.HashMap;
 
@@ -32,7 +12,7 @@ import static junit.framework.Assert.assertFalse;
 import static org.odk.collect.android.preferences.AdminKeys.KEY_EDIT_SAVED;
 import static org.odk.collect.android.preferences.GeneralKeys.KEY_COMPLETED_DEFAULT;
 
-@RunWith(AndroidJUnit4.class)
+@RunWith(RobolectricTestRunner.class)
 public class SharedPreferencesTest {
 
     @Test
