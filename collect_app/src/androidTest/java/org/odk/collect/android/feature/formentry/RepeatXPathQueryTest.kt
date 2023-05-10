ab428fffc8bd775cd48217ba49662bb98ec5da65
Callum Stott <callum@seadowg.com>
2023-05-10 14:56:04 +0300

    Add failing test for caching between versions case

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/RepeatXPathQueryTest.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/RepeatXPathQueryTest.kt
@@ -6,6 +6,9 @@ import org.javarosa.measure.Measure
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
+import org.odk.collect.android.R
+import org.odk.collect.android.support.pages.MainMenuPage
+import org.odk.collect.android.support.pages.ProjectSettingsPage
 import org.odk.collect.android.support.rules.CollectTestRule
 import org.odk.collect.android.support.rules.TestRuleChain
 import java.util.Arrays.asList
@@ -23,21 +26,51 @@ class RepeatXPathQueryTest {
         rule.startAtMainMenu()
             .copyForm("select-from-file-multi-calc.xml", listOf("external.csv"))
             .let {
-                val eqEvals = Measure.withMeasure(asList("PredicateEvaluation", "IndexEvaluation")) {
-                    it.startBlankForm("select-from-file-50k")
-                        .assertQuestion("Select a thing")
-                        .openSelectMinimalDialog()
-                        .clickOnText("My cool label 1")
-                        .swipeToNextQuestion("col1 1col2 1col3 1col4 1col5 1col6 1col7 1col8 1col9 1col10 1")
-                        .pressBackAndDiscardForm()
-                        .startBlankForm("select-from-file-50k")
-                        .assertQuestion("Select a thing")
-                        .openSelectMinimalDialog()
-                        .clickOnText("My cool label 1")
-                        .swipeToNextQuestion("col1 1col2 1col3 1col4 1col5 1col6 1col7 1col8 1col9 1col10 1")
-                }
+                val eqEvals =
+                    Measure.withMeasure(asList("PredicateEvaluation", "IndexEvaluation")) {
+                        it.startBlankForm("select-from-file-50k")
+                            .assertQuestion("Select a thing")
+                            .openSelectMinimalDialog()
+                            .clickOnText("My cool label 1")
+                            .swipeToNextQuestion("col1 1col2 1col3 1col4 1col5 1col6 1col7 1col8 1col9 1col10 1")
+                            .pressBackAndDiscardForm()
 
-                assertThat(eqEvals, lessThan(2 * 2)) // Less than 2 * number of items in external.csv evaluations
+                            .startBlankForm("select-from-file-50k")
+                            .assertQuestion("Select a thing")
+                            .openSelectMinimalDialog()
+                            .clickOnText("My cool label 1")
+                            .swipeToNextQuestion("col1 1col2 1col3 1col4 1col5 1col6 1col7 1col8 1col9 1col10 1")
+                    }
+
+                // Less than 2 * number of items in external.csv evaluations
+                assertThat(eqEvals, lessThan(2 * 2))
             }
+    }
+
+    @Test
+    fun cachingDoesNotCarryOverBetweenFormVersions() {
+        rule.startAtMainMenu()
+            .openProjectSettingsDialog()
+            .clickSettings()
+            .clickFormManagement()
+            .clickOnString(R.string.hide_old_form_versions_setting_title)
+            .pressBack(ProjectSettingsPage())
+            .pressBack(MainMenuPage())
+
+            .copyForm("select-from-file-multi-calc.xml", listOf("external.csv"))
+            .copyForm("select-from-file-multi-calc-updated.xml", listOf("external-updated.csv"))
+
+            .startBlankForm("select-from-file-50k")
+            .assertQuestion("Select a thing")
+            .openSelectMinimalDialog()
+            .clickOnText("My cool label 1")
+            .swipeToNextQuestion("col1 1col2 1col3 1col4 1col5 1col6 1col7 1col8 1col9 1col10 1")
+            .pressBackAndDiscardForm()
+
+            .startBlankForm("select-from-file-50k-updated")
+            .assertQuestion("Select a thing")
+            .openSelectMinimalDialog()
+            .clickOnText("My cool label 1")
+            .swipeToNextQuestion("col1 1col2 1col3 1col4 1col5 1col6 1col7 1col8 1col9 1col10 1")
     }
 }
