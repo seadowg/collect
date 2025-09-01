e9b23bb751a09104c35dcac19f2f7712c32394b0
Callum Stott <callum@seadowg.com>
2025-09-01 17:12:15 +0100

    Allow FormEntryFragment to construct AudioRecordingControllerFragment

--- collect_app/src/main/java/org/odk/collect/android/formhierarchy/FormEntryFragment.java
+++ collect_app/src/main/java/org/odk/collect/android/formhierarchy/FormEntryFragment.java
@@ -1,6 +1,45 @@
 package org.odk.collect.android.formhierarchy;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import org.odk.collect.android.R;
+import org.odk.collect.android.audio.AudioRecordingControllerFragment;
+import org.odk.collect.android.databinding.FormEntryBinding;
+import org.odk.collect.androidshared.ui.FragmentFactoryBuilder;
 
 public class FormEntryFragment extends Fragment {
+
+    private final ViewModelProvider.Factory viewModelFactory;
+
+    public FormEntryFragment(ViewModelProvider.Factory viewModelFactory) {
+        super(R.layout.form_entry);
+        this.viewModelFactory = viewModelFactory;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        getChildFragmentManager().setFragmentFactory(new FragmentFactoryBuilder()
+                .forClass(
+                        AudioRecordingControllerFragment.class,
+                        () -> new AudioRecordingControllerFragment(viewModelFactory)
+                )
+                .build()
+        );
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return FormEntryBinding.inflate(inflater, container, false).getRoot();
+    }
 }
--- collect_app/src/main/java/org/odk/collect/android/formhierarchy/FormEntryFragmentHostActivity.kt
+++ collect_app/src/main/java/org/odk/collect/android/formhierarchy/FormEntryFragmentHostActivity.kt
@@ -2,14 +2,126 @@ package org.odk.collect.android.formhierarchy
 
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import org.odk.collect.analytics.Analytics
 import org.odk.collect.android.R
+import org.odk.collect.android.activities.FormEntryViewModelFactory
+import org.odk.collect.android.entities.EntitiesRepositoryProvider
+import org.odk.collect.android.formentry.FormOpeningMode
+import org.odk.collect.android.formentry.FormSessionRepository
 import org.odk.collect.android.injection.DaggerUtils
+import org.odk.collect.android.instancemanagement.InstancesDataService
+import org.odk.collect.android.instancemanagement.autosend.AutoSendSettingsProvider
+import org.odk.collect.android.projects.ProjectsDataService
+import org.odk.collect.android.utilities.ChangeLockProvider
+import org.odk.collect.android.utilities.FormsRepositoryProvider
+import org.odk.collect.android.utilities.InstancesRepositoryProvider
+import org.odk.collect.android.utilities.MediaUtils
+import org.odk.collect.android.utilities.SavepointsRepositoryProvider
+import org.odk.collect.androidshared.ui.FragmentFactoryBuilder
+import org.odk.collect.async.Scheduler
+import org.odk.collect.audiorecorder.recording.AudioRecorder
+import org.odk.collect.location.LocationClient
+import org.odk.collect.permissions.PermissionsChecker
+import org.odk.collect.permissions.PermissionsProvider
+import org.odk.collect.printer.HtmlPrinter
+import org.odk.collect.qrcode.zxing.QRCodeCreatorImpl
+import org.odk.collect.settings.SettingsProvider
 import org.odk.collect.strings.localization.LocalizedActivity
+import javax.inject.Inject
 
 class FormEntryFragmentHostActivity : LocalizedActivity() {
 
+    @Inject
+    lateinit var scheduler: Scheduler
+
+    @Inject
+    lateinit var formSessionRepository: FormSessionRepository
+
+    @Inject
+    lateinit var mediaUtils: MediaUtils
+
+    @Inject
+    lateinit var analytics: Analytics
+
+    @Inject
+    lateinit var audioRecorder: AudioRecorder
+
+    @Inject
+    lateinit var projectsDataService: ProjectsDataService
+
+    @Inject
+    lateinit var entitiesRepositoryProvider: EntitiesRepositoryProvider
+
+    @Inject
+    lateinit var permissionsChecker: PermissionsChecker
+
+    @Inject
+    lateinit var fusedLocationClient: LocationClient
+
+    @Inject
+    lateinit var settingsProvider: SettingsProvider
+
+    @Inject
+    lateinit var permissionsProvider: PermissionsProvider
+
+    @Inject
+    lateinit var autoSendSettingsProvider: AutoSendSettingsProvider
+
+    @Inject
+    lateinit var instancesRepositoryProvider: InstancesRepositoryProvider
+
+    @Inject
+    lateinit var formsRepositoryProvider: FormsRepositoryProvider
+
+    @Inject
+    lateinit var savepointsRepositoryProvider: SavepointsRepositoryProvider
+
+    @Inject
+    lateinit var instancesDataService: InstancesDataService
+
+    @Inject
+    lateinit var changeLockProvider: ChangeLockProvider
+
+    private lateinit var sessionId: String
+    private val viewModelFactory by lazy {
+        FormEntryViewModelFactory(
+            this,
+            FormOpeningMode.EDIT_SAVED,
+            sessionId,
+            scheduler,
+            formSessionRepository,
+            mediaUtils,
+            audioRecorder,
+            projectsDataService,
+            entitiesRepositoryProvider,
+            settingsProvider,
+            permissionsChecker,
+            fusedLocationClient,
+            permissionsProvider,
+            autoSendSettingsProvider,
+            formsRepositoryProvider,
+            instancesRepositoryProvider,
+            savepointsRepositoryProvider,
+            QRCodeCreatorImpl(),
+            HtmlPrinter(),
+            instancesDataService,
+            changeLockProvider
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         DaggerUtils.getComponent(this).inject(this)
+
+        sessionId = if (savedInstanceState == null) {
+            formSessionRepository.create()
+        } else {
+            savedInstanceState.getString(KEY_SESSION_ID)!!
+        }
+
+        this.supportFragmentManager.fragmentFactory = FragmentFactoryBuilder()
+            .forClass(FormEntryFragment::class.java) { FormEntryFragment(viewModelFactory) }
+            .build()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.form_entry_host_layout)
 
@@ -19,5 +131,9 @@ class FormEntryFragmentHostActivity : LocalizedActivity() {
 
         navController.setGraph(R.navigation.form_entry)
         setSupportActionBar(findViewById(org.odk.collect.androidshared.R.id.toolbar))
+    }
+
+    companion object {
+        private const val KEY_SESSION_ID = "session_id"
     }
 }
